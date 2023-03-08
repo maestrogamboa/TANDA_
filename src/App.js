5a04edfd6ed81ae3cb51e29873c4eee50a9a25be
Edgard Gamboa <Edgard.Gamboa@imail.org>
2023-03-08 00:28:37 -0700

    intialize Auth0

--- src/App.js
+++ src/App.js
@@ -6,17 +6,32 @@ import TellUsMore from "./components/TellUsMore";
 import Signup from "./components/Signup";
 import AddParticipantsCopy from "./components/AddParticipantsCopy";
 import DashBoard from "./components/DashBoard";
+import auth0 from 'auth0-js';
 
 export default function app() {
+
+  let webAuthLogOut = new auth0.WebAuth({
+    domain: process.env.REACT_APP_DOMAIN,
+    clientID: process.env.REACT_APP_CLIENT_ID,
+    redirectUri: 'http://localhost:3000',
+    
+  });
+  let webAuthLogIn = new auth0.WebAuth({
+    domain: process.env.REACT_APP_DOMAIN,
+    clientID: process.env.REACT_APP_CLIENT_ID,
+    redirectUri: 'http://localhost:3000/dashboard',
+    responseType: 'token'
+  });
+
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="" element={<Landing/>}/>
+          <Route path="" element={<Landing webAuthLogIn={webAuthLogIn}/>}/>
           <Route path="/tellusmore" element={<TellUsMore/>}/>
           <Route path="/signup" element={<Signup/>}/>
           <Route path="/addparticipants" element={<AddParticipantsCopy/>}/>
-          <Route path="/dashboard" element={<DashBoard/>}/>
+          <Route path="/dashboard" element={<DashBoard webAuthLogOut={webAuthLogOut}/>}/>
         </Routes>
       </BrowserRouter>
     </div>
